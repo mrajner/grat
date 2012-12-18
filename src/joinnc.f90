9e4ca14e54f1a44d476875c568e20db8665ec683
Marcin Rajner PW <mrajner@gik.pw.edu.pl>
2012-12-18 15:53:34 +0100

    added joinnc

---
+++ src/joinnc.f90
@@ -0,0 +1,41 @@
+program joinnc
+  use get_cmd_line
+  use mod_data
+  use netcdf
+
+  character (80) :: filename, dummy
+  integer :: idimid(2)
+
+  filename = ".filelist"
+
+  print * ,"joining files in ", filename
+  call intro("")
+!  
+  call system ("ls "//trim(model(1)%name)//" > "// trim(filename) )
+
+  open(newunit = iunit, file = filename, action = "read")
+  do 
+    read (iunit , '(a)' , iostat = io_stat) dummy
+    if (io_stat == iostat_end ) exit
+    print * , trim(dummy)
+
+    
+    call check ( nf90_open (dummy , nf90_nowrite , incid) )
+    
+    call check ( nf90_inquire (incid , idim , ivar ,  iatt , iunlimited , iform))
+    call check( nf90_inq_dimid(incid, "lat_0" , idimid(1) ))
+    call check( nf90_inq_dimid(incid, "lon_0" , idimid(2) ))
+
+    print * , idimid
+
+    
+    
+    
+    
+    
+    
+
+  enddo
+
+
+end program
