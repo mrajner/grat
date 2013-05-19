bb324cd49b20061d08d6d72e1b1f30ed2c0c5e75
Marcin Rajner lenovo <mrajner@localhost.localdomain>
2013-05-19 22:32:48 +0200

    ...

--- src/grat.f90
+++ src/grat.f90
@@ -79,6 +79,7 @@ program grat
   do i =1 , size(info)
     print * , info(i)%distance%start
     print * , info(i)%distance%stop
+    print * , info(i)%distance%step
     print * , info(i)%azimuth%start
     print * , info(i)%azimuth%stop
     print * , info(i)%interpolation
