6e0832d809de8efbc0c280d6d9a91c48eea3337d
Marcin Rajner i686 <mrajner@gik.pw.edu.pl>
2013-08-07 15:42:08 +0200

    ...

--- src/barometric_formula.f90
+++ src/barometric_formula.f90
@@ -12,40 +12,18 @@ program check_transfer
 
   nheight=25
   allocate(heights(nheight))
-  heights = linspace(real(0,dp),real(60000,dp),nheight)
+  heights = linspace(real(0,dp),real(30000,dp),nheight)
 
   do i = 1 , nheight
 
-    !  if (i.gt.1) call standard_pressure(h(i), pressure3 , h_zero = h(i-1) ,p_zero = pressure )
-    !  if (i.gt.1) call standard_pressure(h(i), pressure4 , h_zero = h(i-1) ,p_zero = pressure2 )
-
-    !  call standard_pressure(h(i), pressure , if_simplificated=.true.)
-    !  call standard_pressure(h(i), pressure2)
-    ! 
-
-    !  print '(10f10.3)' ,h(i) , pressure , pressure2 , pressure3 ,pressure4
-    !  enddo
-
-    !  call standard_temperature(dble(25.) , temperature)
-    !  print *, temperature
-
-    !  x=1.
-    !  call standard_pressure(x, pressure)
-    !  print * ,x , pressure 
-
-    !  call standard_pressure(x+x2, pressure2)
-    !  print * ,x+x2 , pressure2 
-
-    !  call standard_temperature(x, temperature)
-    !  call standard_pressure(x2, pressure2, p_zero = pressure, t_zero= temperature, if_simplificated=.true.)
-    !  print * ,x+x2 , pressure2 , temperature
 
     if (i.eq.1) print '(10a18)' , "h", "--", "berg", "simple" , "full"
     print '(10f18.9)' , heights(i),  &
       standard_pressure(heights(i)), &
       standard_pressure(heights(i),method="berg") , &
       standard_pressure(heights(i),method="simple"), & 
-      standard_pressure(heights(i),method="full") 
+      standard_pressure(heights(i),method="full",dz=dble(1)), &
+      standard_pressure(heights(i),method="full",dz=dble(100))
   enddo
 
 
