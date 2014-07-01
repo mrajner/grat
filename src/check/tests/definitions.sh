e446f6410a2daa01397d866257b567bd2c84f49a
Marcin Rajner pw <mrajner@gik.pw.edu.pl>
2014-07-01 14:41:19 +0200

    ...

--- src/check/tests/definitions.sh
+++ src/check/tests/definitions.sh
@@ -7,11 +7,12 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-VT="../data/air.2012.nc      @VT : air "
-GP="../data/hgt.2012.nc      @GP : hgt"
-SP="../data/pres.sfc.2012.nc @SP : pres"
-LS="../data/land.nc          @LS : land"
-HP="../data/hgt.sfc.nc       @HP : hgt"
- H="../data/hgt.sfc.nc       @H  : hgt"
- T="1000 @T"
+ VT="../data/air.2012.nc      @VT : air "
+ GP="../data/hgt.2012.nc      @GP : hgt"
+ SP="../data/pres.sfc.2012.nc @SP : pres"
+ LS="../data/land.nc          @LS : land"
+ HP="../data/hgt.sfc.nc       @HP : hgt"
+  H="../data/hgt.sfc.nc       @H  : hgt"
+VSH="../data/shum.2012.nc     @VSH: shum"
+  T="1000 @T"
 
--- src/check/tests/t9.sh
+++ src/check/tests/t9.sh
@@ -9,23 +9,15 @@ set -o nounset                              # Treat unset variables as an error
 
 . definitions.sh
 
-grat                         \
-  -M 1,2,3                       \
-  -G rajner@GN , merriam @GE \
-  -F $SP, $GP , $LS , $HP , $H , $VT , $T , 1e-4 @VSH , 101300 @ RSP , 0 @HRSP  \
-  -U          \
-  -D m:m:10@D  \
-  -Sj -BI  -H -I500@HS \
-  -o ${0/.sh/.tmp} :nc 
-
-grat                         \
-  -! \
-  -M 1,2,3                       \
-  -G rajner@GN , merriam @GE \
-  -F $SP, $GP , $LS , $HP , $H , $VT , $T , 1e-4 @VSH , 101300 @ RSP , 0 @HRSP  \
-  -U          \
-  -D m:m:10@D  \
-  -Sj -BI  -H -I500@HS \
-  -o ${0/.sh/.tmp} :nc 
-
-
+for exclamation in "" "-!" ; do
+  grat                                                                           \
+    ${exclamation} \
+    -M 1,2,3                                                                     \
+    -G rajner@GN , merriam @GE                                                   \
+    -F $SP, $GP , $LS , $HP , $H , $VT , $T , $VSH , 101300 @ RSP , 0 @HRSP \
+    -U                                                                           \
+    -D m:m:210@D                                                                  \
+    -Sj -BI  -H -I500@HS                                                         \
+    -o ${0/.sh/.tmp} :n  \
+    -V
+done
