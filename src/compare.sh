f6f68a5dfcf160edb3cf55bcdae042fd492c5c8f
Marcin Rajner lenovo <mrajner@gik.pw.edu.pl>
2014-09-12 08:00:37 +0200

    ...

--- src/compare.sh
+++ src/compare.sh
@@ -1,8 +1,8 @@
 make FC=ifort 1>/dev/null 2>&1 
-make FC=ifort 1>if 2>&1 | tee if >/dev/null
+make FC=ifort 1>if 2>&1 
 
 make FC=gfortran 1>/dev/null 2>&1
-make FC=gfortran 2>&1 | tee gf >/dev/null
+make FC=gfortran 1>gf 2>&1
 
 
       do_not_compare_list='com\|FF\|^\(real\|user\|sys\)\|\(Unknown[[:space:]]*\)\{3\}\|Program started\|eta *[[:digit:]]\|[Ee]xecution time\|^#[[:space:]]\+v[[:digit:]]\|^#[[:space:]]\+compiled on\|^#[[:space:]]\+compiler:\|FFLAGS\|| %: *[[:digit:]]'
