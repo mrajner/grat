1faf2d0feb29e35b8717804908910bc7a2134d95
Marcin Rajner lidka_virtual <mrajner@gik.pw.edu.pl>
2014-09-13 20:58:12 +0200

    ...

--- src/test/tests/time.sh
+++ src/test/tests/time.sh
@@ -7,11 +7,11 @@
 
 shopt -s nullglob
 
-OUT=${0/.sh/.dat}
-
-
 VERSION=$(grat -v  | sed -n  -e 's/#\s*//g' -e 's/-[^-]*//2' -e 3p | tr "\n" " ")
 OPT=$(grat -v  | sed -n  -e 's/#\s*//g' -e 's/\(.*-O\)\([0-9]\).*/\2/' -e 7p)
+COMPILER=$(grat -v  | grep "compiler:" | sed 's/\(^.*compiler: \)\([^[[:space:]]*\).*/\2/')
+
+cd $GS ; make clean  ; make all FFLAGS=-00 FC=ifort link ; cd - ; make clean 
 
 {      \
   time \
@@ -19,7 +19,14 @@ OPT=$(grat -v  | sed -n  -e 's/#\s*//g' -e 's/\(.*-O\)\([0-9]\).*/\2/' -e 7p)
     make 
     echo TOTAL
   }  ; } 2>&1 \
-    | awk -v version=$VERSION -v opt=$OPT -v date="$(date)" -v host=$HOSTNAME '
+    | awk \
+    -v version=$VERSION \
+    -v opt=$OPT \
+    -v date="$(date)" \
+    -v host=$HOSTNAME \
+    -v comp=$COMPILER \
+    ' 
+  @include "/home/mrajner/.local/src/awk/color.awk"
   {
     if(/^time.**bash/ || /^TOTAL/){
       FILE[++i] = gensub(/(.*bash\s*)(.*\.sh [crs])(.*)/,"\\2","g",$0) 
@@ -32,8 +39,8 @@ OPT=$(grat -v  | sed -n  -e 's/#\s*//g' -e 's/\(.*-O\)\([0-9]\).*/\2/' -e 7p)
 
   END {
   for (i=1;i<=length(FILE);i++){
-    printf "%10s %10s %1i %10s %10s %10s %10s\n",
-    host, version, opt, FILE[i] , TIME[i]["real"], TIME[i]["user"] , TIME[i]["sys"]
+    printf "%10s %6s %10s %1i %10s %10s %10s %10s\n",
+    host, comp, version, opt, FILE[i] , TIME[i]["real"], TIME[i]["user"] , TIME[i]["sys"]
   }
-}'
+}' | tee -a time.dat
 
