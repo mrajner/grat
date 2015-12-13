ab1af85553a534505c363f6708b36c0269a1330d
Marcin Rajner grat <mrajner@gik.pw.edu.pl>
2015-12-13 20:49:35 +0100

    ...

--- src/test/tests/t_3_fortran.f90
+++ src/test/tests/t_3_fortran.f90
@@ -4,6 +4,6 @@ program unit_tests
   print*, "count_separator", count_separator("dfdfsffssfsff,fdfdfdf,fdfsdf,f,dsf,sdfsdfsdfs,,sdffsdf")
   print*, "count_separator", count_separator("-Fsp@SP:1:2:3:4:5:6")
   print*, "count_separator", count_separator("-Fsp@SP:1:2:3:4:5:6",":")
-
+  print*, "count_separator", count_separator("-Fsp@SP:x:y:z:",":")
 
 end program
--- src/tmp.makefile
+++ src/tmp.makefile
@@ -12,5 +12,4 @@ OPT = -O0
 	./$@  ; \
 	} || :
 
-
 .PHONY: gfortran ifort
