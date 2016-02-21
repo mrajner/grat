3c592152859465a42353e1e98e98dda75916447b
Marcin Rajner lenovo <mrajner@gik.pw.edu.pl>
2016-02-21 20:07:31 +0100

    ...

--- src/mod_normalization.f90
+++ src/mod_normalization.f90
@@ -10,9 +10,10 @@ contains
 elemental function green_normalization(method, psi)
   use mod_constants, only: pi, earth, gravity, dp
   use mod_utilities, only: d2r
-  real(dp):: green_normalization
-  character(*) :: method
-  real(dp), optional :: psi
+
+  real(dp) :: green_normalization
+  character(*), intent(in) :: method
+  real(dp), intent(in), optional :: psi
 
   if (method.eq."f2m") then
     green_normalization =                                                     &
@@ -24,8 +25,7 @@ elemental function green_normalization(method, psi)
         psi * 1e15 * earth%radius**2 * 2 * pi * (1.- cos(d2r(1._dp)))
 
   else if (method.eq."f") then ! farrell normalization
-    green_normalization =         &
-        psi * 1e18 * earth%radius
+    green_normalization = psi * 1e18 * earth%radius
   endif
 end function
 
