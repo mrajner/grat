5bd50606007048ea81d2d000e81b8ed493ab5e25
Marcin Rajner lidka <mrajner@gik.pw.edu.pl>
2016-06-29 23:29:53 +0200

    ...

--- src/test/tests/t_4_fortran.f90
+++ src/test/tests/t_4_fortran.f90
@@ -5,20 +5,11 @@ program unit_tests
   print *, 'testing'
   call spher_trig(real(1,dp),2._dp, 1._dp,1._dp,a,b)
   print*, a, b
-  call spher_trig(real(0,dp),0._dp, 1._dp,0._dp,a,b)
+  call spher_trig(real(0,dp),-1._dp, 1._dp,0._dp,a,b, .false.)
+  print*, a, b
+  call spher_trig(real(0,dp),-1._dp, 1._dp,0._dp,a,b, .true.)
   print*, a, b
 
-
-  print*, tmp()
-  c = tmp()
-  print*, c(1), c
-
-contains
-pure function tmp()
-  real(dp), dimension(2) :: tmp
-  tmp(1) = 1._dp
-  tmp(2) = 2._dp
-end function
 
 end program
 
