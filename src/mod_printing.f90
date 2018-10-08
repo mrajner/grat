5bc06a92c3c3945ac52c4f3d5bbdf455b57fd9c4
Marcin Rajner <mrajner@chalmers>
2018-10-08 17:04:20 +0200

    give Chalmers some credit

--- src/mod_printing.f90
+++ src/mod_printing.f90
@@ -274,8 +274,12 @@ subroutine print_version ( &
   write(log%unit, form_inheader) 'FFLAGS = '//fflags
   write(log%unit, form_inheader) "compiled on "//trim(host)//" "//cdate
   write(log%unit, form_inheader) ''
-  write(log%unit, form_inheader) 'Copyright 2013-2018 by Marcin Rajner'
+  write(log%unit, form_inheader) 'Copyright 2013-2017 by Marcin Rajner'
   write(log%unit, form_inheader) 'Warsaw University of Technology'
+  write(log%unit, form_inheader) ''
+  write(log%unit, form_inheader) 'Copyright 2017-2018 by Marcin Rajner'
+  write(log%unit, form_inheader) 'Chalmers University of Technology'
+  write(log%unit, form_inheader) ''
   write(log%unit, form_inheader) 'License: GPLv3 or later'
   write(log%unit, form_header)
 end subroutine
