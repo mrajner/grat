a5de747ac82c2827dab6d6be08d56536467e005b
Marcin Rajner <mrajner@lenovo>
2018-10-08 23:38:29 +0200

    ...

---
+++ src/test/tests/t15.sh
@@ -0,0 +1,19 @@
+#!/bin/bash - 
+#===============================================================================
+#          FILE: t14.sh
+#        AUTHOR: mrajner (mrajner@gik.pw.edu.pl)
+#       CREATED: 02.10.2014 15:38
+#===============================================================================
+
+set -e
+set -o nounset 
+set -o pipefail
+
+. definitions.sh 
+
+{
+  grat \
+    -S j \
+    -F ${EWT-100@SP} \
+    -M2 -G ../../../dat/merriam_green.dat @GR : 1 : 6 @ m2f
+} | tee ${0/.sh/.dat}${suffix}
