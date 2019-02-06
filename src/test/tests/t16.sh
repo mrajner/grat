5d3da3e556c671440ddcebf9eb66854a7c270d45
Marcin Rajner <mrajner@chalmers>
2019-02-06 16:45:54 +0100

    ...

--- src/test/tests/t16.sh
+++ src/test/tests/t16.sh
@@ -1,14 +1,13 @@
-
 . definitions.sh
 
-echo ${SP}
+{
+  # default interpolation is nearest
+  value_check -F ${SP} -S onsala -o:free
+  value_check -F ${SP} -S onsala -In@I -o:free
+  value_check -F ${SP} -S onsala -Il@I -o:free
 
-# default interpolation is nearest
-value_check -F ${SP} -S onsala -o:free
-value_check -F ${SP} -S onsala -In@I -o:free
-value_check -F ${SP} -S onsala -Il@I -o:free
-
-echo
-# with -! -I l@I select only small necessary subarray for interpolation
-value_check -! -F ${SP} -S onsala -In@I -o:free
-value_check -! -F ${SP} -S onsala -Il@I -o:free
+  echo
+  # with -! -I l@I select only small necessary subarray for interpolation
+  value_check -! -F ${SP} -S onsala -In@I -o:free
+  value_check -! -F ${SP} -S onsala -Il@I -o:free
+} | tee ${0/.sh/.dat}${suffix}
