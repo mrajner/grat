c8f695ee11ac1a3304859b1cdc1c893c579ca523
Marcin Rajner <mrajner@chalmers>
2019-02-12 10:34:12 +0100

    ...

--- src/test/tests/TODO_why_difference.sh
+++ src/test/tests/TODO_why_difference.sh
@@ -11,3 +11,9 @@ command='
  eval grat $command
  echo
  eval grat "-!"  $command
+
+  -S  wettzell  : 49 : 12
+  -F  ~/dat/erainterim/sp.2014.nc @ EWT : sp @ pascal2mmwater , ~/dat/landsea/landmaskRg0.25.nc @LS
+  -D m
+  -M 2                                       
+  -G ~/src/gotic2/data/grn1.data@GR : 1 : 2  '
--- src/test/tests/t19.sh
+++ src/test/tests/t19.sh
@@ -2,10 +2,10 @@
 . definitions.sh 
 
 {
-  grat -F $SP -M2 -G rajner@GN : 1 : 3  -Sj
-  grat -F $SP -M2 -G rajner@GN : 1 : 4  -Sj
-  grat -F $SP -M2 -G rajner@GN : 1 : 22  -Sj
-  grat -F $SP -M2 -G ../../../dat/rajner_green.dat@GN : 1 : 3  -Sj
-  grat -F $SP -M2 -G ../../../dat/rajner_green.dat@GN : 1 : 4  -Sj
-  grat -F $SP -M2 -G ../../../dat/rajner_green.dat@GN : 1 : 22  -Sj
+  grat -F $SP -I1@DE -M2 -G rajner@GN : 1 : 3  -Sj
+  grat -F $SP -I1@DE -M2 -G rajner@GN : 1 : 4  -Sj
+  grat -F $SP -I1@DE -M2 -G rajner@GN : 1 : 22  -Sj
+  grat -F $SP -I1@DE -M2 -G ../../../dat/rajner_green.dat@GN : 1 : 3  -Sj
+  grat -F $SP -I1@DE -M2 -G ../../../dat/rajner_green.dat@GN : 1 : 4  -Sj
+  grat -F $SP -I1@DE -M2 -G ../../../dat/rajner_green.dat@GN : 1 : 22  -Sj
 } | tee ${0/.sh/.dat}${suffix}
