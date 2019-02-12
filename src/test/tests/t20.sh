de1bd0a4af595df13412b42874b693038b74d0ee
Marcin Rajner <mrajner@lenovo>
2019-02-12 23:45:09 +0100

    ...

--- src/test/tests/t20.sh
+++ src/test/tests/t20.sh
@@ -1,14 +1,14 @@
 
-. definitions.sh s
+. definitions.sh
 
 {
-  # $G/bin/grat -F $SP   -S j -M1 -A1 -Dm:m
-  # $G/bin/grat -F ../data/ncks.nc      @SP : sp -S j -M1 -A1 -Dm:m
-  $G/bin/value_check -F $SP , ../data/ncks.nc @SP : sp -S j -M1  -Dm:m
+  grat -F $SP   -S j -M1 -A1 -Dm:m
+  grat -F ../data/ncks.nc      @SP : sp -S j -M1 -A1 -Dm:m
+  value_check -F $SP , ../data/ncks.nc @SP : sp -S j -M1  -Dm:m
   echo
 
-  # $G/bin/grat        -! -F $SP             -S  j               -M1 -A1 -Dm:m
-  # $G/bin/grat        -! -F ../data/ncks.nc @SP :               sp  -S  j  -M1 -A1 -Dm:m
-  $G/bin/value_check -! -F $SP             ,   ../data/ncks.nc @SP :   sp -S  j   -M1 -Dm:m
+  grat        -! -F $SP             -S  j               -M1 -A1 -Dm:m
+  grat        -! -F ../data/ncks.nc @SP :               sp  -S  j  -M1 -A1 -Dm:m
+  value_check -! -F $SP             ,   ../data/ncks.nc @SP :   sp -S  j   -M1 -Dm:m
   
 } | tee ${0/.sh/.dat}${suffix}
