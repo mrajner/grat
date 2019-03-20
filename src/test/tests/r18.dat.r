1c6215389af40af936c0ad7f5fd5af0d0c7d571c
Marcin Rajner <mrajner@chalmers>
2019-03-20 12:19:45 +0100

    ...

--- src/test/tests/r18.dat.r
+++ src/test/tests/r18.dat.r
@@ -1,4 +1,2 @@
 warning: -M no method was set assuming 2D 
-55927.000 20120101000000   joze_a   52.0000   21.0000   110.000error: mask need to be <0,1> or NaN 
-Note: The following floating-point exceptions are signalling: IEEE_INVALID_FLAG
-STOP 1
+55927.000 20120101000000   joze_a   52.0000   21.0000   110.000 -414.051E+00
