f8aaced7d35520b0638c39ecf68010d6897a887d
Marcin Rajner <mrajner@chalmers>
2019-03-20 18:10:27 +0100

    ...

--- src/test/tests/r11.dat1.r
+++ src/test/tests/r11.dat1.r
@@ -3,6 +3,5 @@ warning: reading whole file with levels into memory could slow down computation
 warning: level not found (no warning again) hgt.2012.nc                                                                                                                                                                                              
 warning: cannot find date: 20090101000000      var: pres in file: ../data/pres.sfc.2012.nc 
 warning: cannot find date: 20040101000000      var: pres in file: ../data/pres.sfc.2012.nc 
-warning: something wrong with -F. non_exisiting_path : file do not exist 
-warning: something wrong with -F. another_one : file do not exist 
-warning: reading whole file with levels into memory could slow down computation 
+STOP 1
+error: something wrong with -F. non_exisiting_path : file do not exist 
