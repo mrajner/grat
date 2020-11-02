07b4dd6c5d70c07c0d8c9cccd72cbb78b4ff3440
Marcin Rajner <mrajner@archx240>
2020-11-02 16:24:25 +0100

    ...

--- src/test/tests/t_fortran.f90
+++ src/test/tests/t_fortran.f90
@@ -3,25 +3,31 @@ program testing_fortran_procedures
   use mod_cmdline
   use mod_aggf
 
+  implicit none
   integer :: iunit
   character(30) :: filename
 
   print *, "output of: ./test"
 
-  call uniq_name_unit(prefix="non_existing_file_",filename=filename,digits=5,unit=iunit,ifcreate=.false.)
+  call uniq_name_unit( &
+    prefix   = "non_existing_file_", &
+    filename = filename,             &
+    digits   = 5,                    &
+    unit     = iunit,                &
+    ifcreate = .false.               &
+    )
   print *, filename
   call uniq_name_unit(filename=filename,start=30,unit=iunit, suffix="_suff",ifcreate=.false.)
   print *, filename
 
   print *, method3dnames
 
-
   call test_standard_atmosphere("full")
   call test_standard_atmosphere("standard")
   call test_standard_atmosphere("berg")
   call test_standard_atmosphere("simple")
 
-  write (*,10) aggf(psi=0.1_dp, method="simple", dz=2._dp)
+  write(*,10) aggf(psi=0.1_dp, method="simple", dz=2._dp)
 
   write(*,10) aggf(psi = 1e-6_dp, method = "full")
   write(*,10) aggf(psi = 1e-6_dp, method = "standard")
